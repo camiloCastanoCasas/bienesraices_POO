6c5d78e881280fe2fbdb1d0e38ac089147f99cab
Camilo Castano <camilo.castano.casas@gmail.com>
2024-06-17 21:30:27 -0500

    Modo Oscuro

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
+    darkMode();
 });
 
 function eventListeners() {
@@ -7,7 +8,27 @@ function eventListeners() {
     mobileMenu.addEventListener('click', navegacionResponsive);
 }
 
+//Funcion para mostrar y ocultar la navegacion en dispositivos moviles
 function navegacionResponsive(){
     const navegacion = document.querySelector('.navegacion');
     navegacion.classList.toggle('mostrar');
+}
+
+//Funcion para activar el modo oscuro
+function darkMode(){
+
+    //Preferencia de modo oscuro en el sistema operativo
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    prefiereDarkMode.matches ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+
+    //Preferencia de modo oscuro en el navegador, cambia en tiempo real.
+    prefiereDarkMode.addEventListener('change', function(){
+        prefiereDarkMode.matches ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+    });
+    
+    //Boton para activar el modo oscuro
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
 }
