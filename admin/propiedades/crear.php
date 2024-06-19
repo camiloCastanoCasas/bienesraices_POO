a7ab766be85f5f31e3a77615d72c9ea7b60121bb
Camilo Castano <camilo.castano.casas@gmail.com>
2024-06-19 18:53:09 -0500

    Saneamiento de datos y subida de archivos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -22,14 +22,17 @@
     
     //Ejecutar el código después de que el usuario envía el formulario
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $nombre = $_POST['nombre'];
-        $precio = $_POST['precio'];
-        $descripcion = $_POST['descripcion'];
-        $habitaciones = $_POST['habitaciones'];
-        $wc = $_POST['wc'];
-        $estacionamiento = $_POST['estacionamiento'];
-        $id_vendedor = $_POST['vendedor'];
+
+        // Sanitizar los datos
+        $nombre = mysqli_real_escape_string($db, $_POST['nombre']);
+        $precio = mysqli_real_escape_string($db, $_POST['precio']);
+        $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+        $habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+        $wc = mysqli_real_escape_string($db, $_POST['wc']);
+        $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+        $id_vendedor = mysqli_real_escape_string($db, $_POST['vendedor']);
         $creado = date('Y/m/d');
+        $imagen = $_FILES['imagen']; //Asignar files a una variable
 
         //Errores
         $tiposErrores = [
@@ -39,7 +42,7 @@
             'habitaciones' => 'Debes añadir el número de habitaciones',
             'wc' => 'Debes añadir el número de baños',
             'estacionamiento' => 'Debes añadir el número de estacionamientos',
-            'id_vendedor' => 'Debes elegir un vendedor'
+            'id_vendedor' => 'Debes elegir un vendedor',
         ];
 
         //Validar que no haya campos vacíos
@@ -49,10 +52,35 @@
             }
         }
 
+        //Validar la imagen
+        //Si no hay imagen
+        if(!$imagen['name'] || $imagen['error']){
+            $errores[] = 'Debes añadir una imagen';
+        }
+        //Validar tamaño de la imagen
+        if($imagen['size'] > 1000 * 2000){
+            $errores[] = 'La imagen es muy pesada';
+        }
+
         //Si no hay errores entonces se procede a insertar en la base de datos
         if(empty($errores)){
+
+            //Subir imagen
+            //Crear carpeta
+            $carpeta = '../../imagenes/';
+
+            if(!is_dir($carpeta)){
+                mkdir($carpeta);
+            }
+            
+            //Generar un nombre único
+            $nombreImagen = md5(uniqid(rand(), true)) . strrchr($imagen['name'], '.');
+
+            //Mover imagen a la carpeta
+            move_uploaded_file($imagen['tmp_name'], $carpeta . $nombreImagen);
+
             //Insertar en la base de datos
-            $query = "INSERT INTO propiedades (nombre, precio, descripcion, habitaciones, wc, estacionamiento, creado, id_vendedor) VALUES ('$nombre', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$id_vendedor')";
+            $query = "INSERT INTO propiedades (nombre, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, id_vendedor) VALUES ('$nombre', '$precio', '$nombreImagen' , '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$id_vendedor')";
 
             $resultado = mysqli_query($db, $query);
 
@@ -78,7 +106,7 @@
             </div>
         <?php endforeach; ?>
 
-        <form action="/admin/propiedades/crear.php" method="POST" class="formulario margen-superior">
+        <form action="/admin/propiedades/crear.php" method="POST" class="formulario margen-superior" enctype="multipart/form-data">
             <fieldset>
                 <legend>Información General</legend>
 
@@ -89,7 +117,7 @@
                 <input type="number" id="precio" name="precio" placeholder="Precio de la propiedad" value="<?php echo $precio?>">
 
                 <label for="imagen">Imagen:</label>
-                <input type="file" id="imagen" accept="image/jpeg, image/png">
+                <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
 
                 <label for="descripcion">Descripción:</label>
                 <textarea id="descripcion" name="descripcion"><?php echo $descripcion?></textarea>
